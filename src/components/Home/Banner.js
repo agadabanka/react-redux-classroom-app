13083817f03be36b3d7c79c4056be93d500068d2
Amith Tudur <amith.amith@gmail.com>
2021-12-02 17:01:14 -0800

    Update text in home page.

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = ({ appName, token }) => {
         <h1 className="logo-font">
           {appName.toLowerCase()}
         </h1>
-        <p>A not so nice place to share your knowledge.</p>
+        <p>A safe place to share your homework and look for answers.</p>
       </div>
     </div>
   );
